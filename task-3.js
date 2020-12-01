c388227fef7e780ac7dfe3ae41482bc3ce710ce8
sBeznosiuk <67642990+sBeznosiuk@users.noreply.github.com>
2020-12-01 21:22:48 +0200

    Create task-3.js

---
+++ task-3.js
@@ -0,0 +1,17 @@
+const auth = prompt()
+const ADMIN_PASSWORD = 'jqueryismyjam';
+let message;
+console.log(auth)
+
+if (auth === null) {
+    message = 'Отменено пользователем!';
+    console.log(message);
+} else if (auth === ADMIN_PASSWORD) {
+    message = 'Добро пожаловать!';
+    console.log(message);
+} else {
+    message = 'Доступ запрещен, неверный пароль!';
+    console.log(message);
+}
+
+alert(message);
