7f6abcb87bcebcc7277750ef009e37ae41a3c941
sBeznosiuk <67642990+sBeznosiuk@users.noreply.github.com>
2020-12-01 21:23:08 +0200

    Create task-4.js

---
+++ task-4.js
@@ -0,0 +1,23 @@
+let quantity = prompt();
+const credits = 23580;
+const pricePerDroid = 3000;
+const totalPrice = quantity * pricePerDroid;
+let message;
+
+console.log(quantity)
+
+if (quantity === null) {
+    message = 'Отменено пользователем!'
+    console.log(message)
+} else {
+    if ((quantity * pricePerDroid) <= credits) {
+        message = `Вы купили ${quantity} дроидов, на счету осталось ${credits - totalPrice} кредитов.`
+        console.log(message)
+        console.log(Number(credits - totalPrice))
+    } else {
+        message = 'Недостаточно средств на счету!';
+        console.log(message)
+    }
+}
+
+alert(message)
